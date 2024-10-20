3cacbac3cfa46d2b9e8ea190bc3dcb0d74efc59a
yuan <yuansir94@gmail.com>
2024-10-21 00:38:42 +0800

    init

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/doc_592222_xyz",
+  base: "/doc_592222_xyz/",
 
   lang: "zh-CN",
   title: "文档演示",
